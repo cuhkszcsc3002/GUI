b53a078fef179366f2e3b80644ef773a335b3549
cuhkszcsc3002 <49808472+cuhkszcsc3002@users.noreply.github.com>
2019-05-10 00:26:08 +0800

    code of chessboard window - still testing

---
+++ chessboard/lib/stones.h
@@ -0,0 +1,15 @@
+#ifndef STONES_H
+#define STONES_H
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class stones{
+
+public:
+
+static vector<string> Stone;
+
+};
+#endif // STONES_H
